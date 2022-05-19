bb105513ff3f6a7c1f87b7417fd37a457436c011
RiceNLamb <mlam7947@uni.sydney.edu.au>
2022-05-19 15:23:38 +1000

    tested the duplicate checker.

--- deliverable/main.cpp
+++ deliverable/main.cpp
@@ -3,11 +3,11 @@
 #include<unistd.h>
 
 using namespace std;
-vector<CardObject> card_values; 
+
 
 
 int main(){
-
+    vector<CardObject> card_values; 
     CardObject first_card;
     CardObject second_card;
     first_card.cardCreator();
@@ -17,12 +17,10 @@ int main(){
 
     duplicate_checker(card_values);
     
-    
 
     
     // vector<string> initial_cards{start_sequence(first_card.card_structure,second_card.card_structure)};
 
     //game_sequence(starting_total, initial_cards);
-
     usleep(3000000);
 }
--- deliverable/processes.h
+++ deliverable/processes.h
@@ -32,9 +32,6 @@ class CardObject {
             return 0;
 }
     public:
-        string values[13] {
-            "A", "2", "3", "4","5", "6", "7", "8", "9", "10", "J","Q","K" 
-        };
         string chosen_suit {""};
         string chosen_value {""};
         tuple<string,string> indv_card_values; 
@@ -147,11 +144,15 @@ void duplicate_checker(vector<CardObject> checking_card_values){
     bool all_clear{false};
     while (all_clear != true){
         for (int i{0}; i < checking_card_values.size(); ++ i){
-            for (int j{i+1}; i < checking_card_values.size(); ++j){
+            cout << i;
+            for (int j{i+1}; j < checking_card_values.size(); ++j){
                 if (checking_card_values[i].indv_card_values == checking_card_values[j].indv_card_values){
                     checking_card_values[j].cardCreator();
                     i = 0;
                     continue;
+                }
+                else{
+
                 }
             }
         }
